bb8b0d6ffc449c7df70aa70ac4e07de1e3140881
dt <dogntao@163.com>
2018-05-02 15:47:56 +0800

    修改目录

--- demo/php/recursion.php
+++ demo/php/recursion.php
@@ -1,55 +1,30 @@
 <?php
-
-function test1(int $n)
+function test($arr, $search)
 {
-    if ($n >0) {
-        echo 'a'.$n . "\r\n";
-        test1($n - 1);
+    $start = 0;
+    $end   = count($arr) - 1;
+    // 遍历次数
+    $count = 0;
+
+    // 开始位置不能大于结束位置
+    while ($start <= $end) {
+        $count++;
+        $mid = floor(($start + $end) / 2);
+        if ($search > $arr[$mid]) {
+            // 搜索值大于中间值，开始位置为中间值+1
+            $start = $mid + 1;
+        } elseif ($search < $arr[$mid]) {
+            // 搜索值小于中间值，结束位置为中间值-1
+            $end = $mid - 1;
+        } else {
+            // 搜索值等于中间值，返回
+            return [$mid, $count];
+        }
     }
-    echo 'c'.$n . "\r\n";
-    return $n;
+    // 没找到返回没找到
+    return $count . "未找到\r\n";
 }
-echo test1(2)."\r\n";
-echo "......\r\n";
-
-function test2(int $n)
-{
-    if ($n > 0) {
-        echo 'a' . $n . "\r\n";
-        $n = test2($n - 1);
-    } else {
-        echo 'b' . $n . "\r\n";
-        return $n;
-    }
-    echo 'c' . $n . "\r\n";
-    return $n;
-}
-echo test2(2)."\r\n";
-echo "......\r\n";
-
-function test3(int $n)
-{
-    if ($n > 0) {
-        echo 'a' . $n . "\r\n";
-        test3($n - 1);
-    } else {
-        echo 'b' . $n . "\r\n";
-        return $n;
-    }
-    echo 'c' . $n . "\r\n";
-    return $n;
-}
-echo test3(2)."\r\n";
-echo "......\r\n";
-
-// 查找出现位置
-function FunctionName()
-{
-    $arr = [1,3,5,7,9];
-    $search = 7;
-    // 取中间数
-    $len = floor(count($arr)/2);
-    $mid = 0;
-    
-    
-}
+$arr = [1, 3, 5, 7, 9];
+print_r(test($arr, 1));
+print_r(test($arr, 2));
+print_r(test($arr, 9));
